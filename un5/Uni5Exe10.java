b585d2a91fbb391e6f99d6b0ffa50c6f96f5ddfa
lgs2005 <lgs22264@gmail.com>
2023-05-15 09:33:19 -0300

    Incredible. Absolutely phenomenal.

--- un5/Uni5Exe10.java
+++ un5/Uni5Exe10.java
@@ -1,22 +1,31 @@
 public class Uni5Exe10 {
     public static void main(String[] args) {
         int numerosEncontrados = 0;
+        int maxNumeros = 10;
+        
+        // um while seria melhor pra resolver isso aq, mas como o exercicio esta na seção de for...
+        // contando até o infinito enquanto menos de 10 numeros encontrados
+        for (int num = 10; numerosEncontrados < maxNumeros; num++) {
+            // para cada numero, conta em potencias de 10 até a potencia ser maior que o numero
+            // n=10, n=100, n=1000, n=10000
+            for (int potencia = 10; potencia <= num && numerosEncontrados < maxNumeros; potencia *= 10) {
+                // divide o numero usando a potencia
+                int esquerda = num % potencia;
+                int direita = (num - esquerda) / potencia;
+                int valor = esquerda + direita;
 
-        for (int i = 10; numerosEncontrados < 10; i++) {
-            for (int n = 10; n <= i && numerosEncontrados < 10; n *= 10) {
-                int y = i % n;
-                int x = (i - y) / n;
-                int s = x + y;
-
-                if (s*s == i) {
-                    System.out.println(x + " + " + y + " = " + s + " -> " + s + "*" + s + " = " + i);
+                if (valor*valor == num) {
+                    System.out.println(direita + " + " + esquerda + " = " + valor + " -> " + valor + "*" + valor + " = " + num);
                     numerosEncontrados += 1;
                 }
             }
         }
 
-        // Usando apenas três digitos
-        // Apenas encontra três soluções
+        // 2025
+        // 3025
+        // 9001
+        // Phenomenal.
+
         // for (int i = 100; numerosEncontrados < 10; i++) {
         //     int y = i % 100;
         //     int x = (i - y)/100;
