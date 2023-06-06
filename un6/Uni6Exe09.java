23d42725079d0769533fd61a61907511799642c5
lgs2005 <lgs22264@gmail.com>
2023-06-06 10:04:15 -0300

    its a ghost dance

---
+++ un6/Uni6Exe09.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Uni6Exe09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] notaMasculino = new int[30];
+        int[] notaFeminino = new int[30];
+
+        
+    }
+}
